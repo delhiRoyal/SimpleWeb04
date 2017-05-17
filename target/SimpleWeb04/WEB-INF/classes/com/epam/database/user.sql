7e4581318b47a95aff7385f3b9832457b182cdd0
Royal Singh <royal_singh@epam.com>
2017-05-17 16:43:51 +0530

    second commit

--- target/SimpleWeb04/WEB-INF/classes/com/epam/database/user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `user_details`.`user` (`u_name`, `u_password`, `r_id`) VALUES ('sarthak', 'f5a0d6e19744068a5f30f0146c54893695e33626', '1');
-INSERT INTO `user_details`.`user` (`u_name`, `u_password`, `r_id`) VALUES ('shubham', 'f5a0d6e19744068a5f30f0146c54893695e33626', '2');
--- target/classes/com/epam/database/role.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `user_details`.`role` (`r_id`, `r_role`) VALUES ('1', 'administrator');
-INSERT INTO `user_details`.`role` (`r_id`, `r_role`) VALUES ('2', 'user');
